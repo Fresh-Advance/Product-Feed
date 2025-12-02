43a91c55aa08d4d0fd1030f98aee9c3d0df39523
Anton Fedurtsya <anton@fedurtsya.com>
2025-12-02 22:17:13 +0200

    Cleanup unnecessary alignments
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- metadata.php
+++ metadata.php
@@ -14,13 +14,13 @@ $sMetadataVersion = '2.1';
  * Module information
  */
 $aModule = [
-    'id'          => 'fa_product_feed',
-    'title'       => 'Product Feed',
+    'id' => 'fa_product_feed',
+    'title' => 'Product Feed',
     'description' => 'Product Feed API for OXID eShop',
-    'thumbnail'   => 'pictures/logo.png',
+    'thumbnail' => 'pictures/logo.png',
     'version' => '1.0.0',
-    'author'      => 'MB Arbatos Klubas',
-    'url'         => 'https://github.com/Fresh-Advance/Product-Feed',
-    'email'       => 'info@fresh-advance.eu',
-    'extend'      => [],
+    'author' => 'MB Arbatos Klubas',
+    'url' => 'https://github.com/Fresh-Advance/Product-Feed',
+    'email' => 'info@fresh-advance.eu',
+    'extend' => [],
 ];
